14058457ff4432ff268e7e0279c91b77d93bd027
Alexis23030 <23030ah@hvhs.school.nz>
2026-03-04 14:09:27 +1300

    4/3/26 - Player Creation

--- game.js
+++ game.js
@@ -5,10 +5,13 @@
 /*******************************************************/
 
 
-
-
-
-//preload func
+/*******************************************************/
+// preload()
+/*******************************************************/
+function preload() {
+  player1img = loadImage('../images/player1.png');
+  player2img = loadImage('../images/player2.png');
+}
 
 
 /*******************************************************/
@@ -44,10 +47,10 @@ function windowDraw() {
 // createPlayer1()
 /*******************************************************/
 function createPlayer1() {
-    player1 = new Sprite(200, 200, 100, 100, 'd')
-    player1.image = (player1.png);
-    // x, y, w, h, phys.
-
+    player1 = new Sprite(width-100, height-100, 50, 50, 'd')
+    player1.image = (player1img);
+    player1.scale = 50 / player1img.width;
+    player1.direction = -90;
 }
 
 
@@ -55,7 +58,10 @@ function createPlayer1() {
 // createPlayer2()
 /*******************************************************/
 function createPlayer2() {
-    //stuff
+    player2 = new Sprite(100, 100, 50, 50, 'd')
+    player2.image = (player2img);
+    player2.scale = 50 / player2img.width;
+    player2.direction = -90;
 }
 
 
@@ -64,6 +70,21 @@ function createPlayer2() {
 /*******************************************************/
 function draw() {
     background('white');
+
+    if (kb.pressing('up')){
+        player1.speed = 4
+    }
+    //if (kb.pressing('down')){
+     //   player1.speed = -4
+   // }
+    if (kb.pressing('left')){
+        player1.direction = player1.direction - 2
+            player1.rotation = player1.rotation - 2
+    }   
+    if (kb.pressing('right')){
+        player1.direction = player1.direction + 2
+        player1.rotation = player1.rotation + 2
+    }   
 }
 
 
